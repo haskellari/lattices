64e34f7e444ea9048d83daa022fa60f862f12f62
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-06-27 02:48:15 +0300

    M3 ja M2 examples

--- test/Tests.hs
+++ test/Tests.hs
@@ -8,8 +8,9 @@ module Main (main) where
 import Prelude ()
 import Prelude.Compat
 
+import Data.Maybe (listToMaybe, isJust)
 import Data.Monoid ((<>))
-import Control.Monad (ap)
+import Control.Monad (ap, guard)
 import Test.QuickCheck.Function
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
@@ -42,7 +43,9 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests"
-  [ latticeLaws "Map" True (Proxy :: Proxy (Map Int (O.Ordered Int)))
+  [ latticeLaws "M3" False (Proxy :: Proxy M3) -- non distributive lattice!
+  , latticeLaws "M2" True (Proxy :: Proxy M2) -- M2
+  , latticeLaws "Map" True (Proxy :: Proxy (Map Int (O.Ordered Int)))
   , latticeLaws "IntMap" True (Proxy :: Proxy (IntMap (O.Ordered Int)))
   , latticeLaws "Set" True (Proxy :: Proxy (Set Int))
   , latticeLaws "IntSet" True (Proxy :: Proxy IntSet)
@@ -50,6 +53,9 @@ tests = testGroup "Tests"
   , latticeLaws "Divisibility" True (Proxy :: Proxy (Div.Divisibility Int))
   , latticeLaws "LexOrdered" True (Proxy :: Proxy (LO.Lexicographic (O.Ordered Int) (O.Ordered Int)))
   , latticeLaws "Lexicographic" False (Proxy :: Proxy (LO.Lexicographic (Set Bool) (Set Bool)))
+  , latticeLaws "Lexicographic" False (Proxy :: Proxy (LO.Lexicographic M2 M2)) -- non distributive!
+  , testProperty "Lexicographic M2 M2 contains M3" $ QC.property $
+      isJust searchM3LexM2
   , monadLaws "Dropped" (Proxy1 :: Proxy1 D.Dropped)
   , monadLaws "Levitated" (Proxy1 :: Proxy1 L.Levitated)
   , monadLaws "Lexicographic" (Proxy1 :: Proxy1 (LO.Lexicographic Bool))
@@ -219,3 +225,125 @@ instance Arbitrary a => Arbitrary (Op.Op a) where
 instance (Arbitrary k, Arbitrary v) => Arbitrary (LO.Lexicographic k v) where
     arbitrary = uncurry LO.Lexicographic <$> arbitrary
     shrink (LO.Lexicographic k v) = uncurry LO.Lexicographic <$> shrink (k, v)
+
+-------------------------------------------------------------------------------
+-- Examples
+-------------------------------------------------------------------------------
+
+-- | Non-distributive lattice
+data M3 = M3_0 | M3_a | M3_b | M3_c | M3_1
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+instance Arbitrary M3 where
+    arbitrary = QC.arbitraryBoundedEnum
+
+instance PartialOrd M3 where
+    x    `leq` y    | x == y = True
+    M3_0 `leq` _             = True
+    _    `leq` M3_1          = True
+    _    `leq` _             = False
+
+instance JoinSemiLattice M3 where
+    x    \/ M3_0          = x
+    M3_0 \/ y             = y
+    _    \/ M3_1          = M3_1
+    M3_1 \/ _             = M3_1
+    x    \/ y | x == y    = x
+              | otherwise = M3_1
+
+instance MeetSemiLattice M3 where
+    x    /\ M3_1          = x
+    M3_1 /\ y             = y
+    _    /\ M3_0          = M3_0
+    M3_0 /\ _             = M3_0
+    x    /\ y | x == y    = x
+              | otherwise = M3_0
+
+instance Lattice M3 where
+
+-- | Set Bool, M2
+data M2 = M2_0 | M2_T | M2_F | M2_1
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+instance Arbitrary M2 where
+    arbitrary = QC.arbitraryBoundedEnum
+
+instance PartialOrd M2 where
+    x    `leq` y   | x == y  = True
+    M2_0 `leq` _             = True
+    _    `leq` M2_1          = True
+    _    `leq` _             = False
+
+instance JoinSemiLattice M2 where
+    x    \/ M2_0          = x
+    M2_0 \/ y             = y
+    _    \/ M2_1          = M2_1
+    M2_1 \/ _             = M2_1
+    x    \/ y | x == y    = x
+              | otherwise = M2_1
+
+instance MeetSemiLattice M2 where
+    x    /\ M2_1          = x
+    M2_1 /\ y             = y
+    _    /\ M2_0          = M2_0
+    M2_0 /\ _             = M2_0
+    x    /\ y | x == y    = x
+              | otherwise = M2_0
+
+instance Lattice M2 where
+
+instance BoundedJoinSemiLattice M2 where
+    bottom = M2_0
+
+instance BoundedMeetSemiLattice M2 where
+    top = M2_1
+
+instance BoundedLattice M2 where
+
+-------------------------------------------------------------------------------
+-- Lexicographic M2 search
+-------------------------------------------------------------------------------
+
+searchM3 :: (Eq a, PartialOrd a, Lattice a) => [a] -> Maybe (a,a,a,a,a)
+searchM3 xs = listToMaybe $ do
+    x0 <- xs
+    xa <- xs
+    guard (xa `notElem` [x0])
+    guard (x0 `leq` xa)
+    xb <- xs
+    guard (xb `notElem` [x0,xa])
+    guard (x0 `leq` xb)
+    guard (not $ comparable xa xb)
+    xc <- xs
+    guard (xc `notElem` [x0,xa,xb])
+    guard (x0 `leq` xc)
+    guard (not $ comparable xa xc)
+    guard (not $ comparable xb xc)
+    x1 <- xs
+    guard (x1 `notElem` [x0,xa,xb,xc])
+    guard (x0 `leq` x1)
+    guard (xa `leq` x1)
+    guard (xb `leq` x1)
+    guard (xc `leq` x1)
+
+    -- homomorphism
+    let f M3_0 = x1
+        f M3_a = xa
+        f M3_b = xb
+        f M3_c = xc
+        f M3_1 = x1
+
+    ma <- [minBound .. maxBound]
+    mb <- [minBound .. maxBound]
+    guard (f (ma /\ mb) == f ma /\ f mb)
+    guard (f (ma \/ mb) == f ma \/ f mb)
+
+    return (x0,xa,xb,xc,x1)
+
+type L2 = LO.Lexicographic M2 M2
+
+searchM3LexM2 :: Maybe (L2,L2,L2,L2,L2)
+searchM3LexM2 = searchM3 xs
+  where
+    xs = [ LO.Lexicographic x y | x <- ys, y <- ys ]
+    ys = [minBound .. maxBound]
