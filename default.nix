5c5ca0a8ef60f5f511867bdfbc6ecdf69bacc4ca
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-05-06 17:15:25 +0300

    Updates: GHC versions in CI, unused dev things

--- default.nix
+++
@@ -1,3 +0,0 @@
-# this configuration is contrib, if it doesn't work anymore, please fix it and submit a PR
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8102" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./lattices.nix { }
